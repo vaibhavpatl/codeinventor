857b8faaf6ba0f1ae85884225953617a0ae45d1f
vaibhavpatl <lavjimasoya@gmail.com>
2022-10-16 18:31:02 +0530

    final commit

--- convex/submitTrip.ts
+++
@@ -1,7 +0,0 @@
-import { mutation } from "./_generated/server";
-
-// Send a chat message.
-export default mutation(({ db }, id: number, date: string, title: string, origin: string, destination: string, distance: number, mode: string, emission: number) => {
-  const trip = { id,  date, title, origin, destination, distance, mode, emission };
-  db.insert("trip", trip);
-});
